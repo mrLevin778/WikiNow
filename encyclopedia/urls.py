692bfe65d85160f8b9fc47e9f3a69c765a6e2033
mrLevin778 <92675096+mrLevin778@users.noreply.github.com>
2022-01-23 21:15:58 +0200

    minor fixes

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path('create/', views.create, name="create"),
-    path('wiki/<str:pagetitle>', views.article, name="article"),
+    path("create/", views.create, name="create"),
+    path("wiki/<str:title>", views.article, name="article"),
+    path("random/", views.randompage, name="randompage"),
+    path("search/", views.search_page, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,24 +1,54 @@
 from django import forms
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
 from . import util
 
+
 class NewPageForm(forms.Form):
-    pagetitle = forms.CharField(max_length=50, label="")
-    pagecontent = forms.CharField(max_length=9000, label="", widget=forms.Textarea)
+    pagetitle = forms.CharField(max_length=50, label="Article name: ")
+    pagecontent = forms.CharField(max_length=9000, label="Article content: ", widget=forms.Textarea)
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=50, label="Search Encyclopedia")
+
+
+def search_page(request):
+    if request.method == "POST":
+        searchform = SearchForm(request.POST)
+        if searchform.is_valid():
+            title = searchform.cleaned_data["search"]
+            return HttpResponseRedirect("encyclopedia/wiki.html", {
+                "article": util.get_entry(title)
+            })
+        else:
+            return HttpResponse("Not Found!")
+    return render(request, "encyclopedia/create.html", {
+        "searchform": SearchForm(),
+    })
 
 
 def index(request):
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+
     })
 
-def article(request, pagetitle):
+
+def article(request, title):
     return render(request, "encyclopedia/wiki.html", {
-        "page": util.get_entry(pagetitle)
+        "article": util.get_entry(title),
+        "title": title
     })
+
+
+def randompage(request):
+
+    return render(request, "encyclopedia/wiki.html")
+
 
 def create(request):
     if request.method == "POST":
